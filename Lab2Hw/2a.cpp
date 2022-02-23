b96551e3da3358efd6566af5f0e396bd6b556134
haynoexe <64045917+haynoten4@users.noreply.github.com>
2022-02-23 21:55:16 +0500

    Delete Lab2Hw directory

--- Lab2Hw/2a.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int *number;
-    number = NULL;
-    cout<<number<<endl;
-}
-// Here we have made the pointer number as null, before this, the pointer was having a garbage value but after making it null,
-// now it has 0. This is the possible solution of the above code
